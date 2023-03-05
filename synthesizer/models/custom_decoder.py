b99584c83f20f0407a075cb56fd39260aaabff9e
sonmyson <maks.medved20031113@gmail.com>
2023-03-05 04:05:02 +0200

    Update custom_decoder.py

--- synthesizer/models/custom_decoder.py
+++ synthesizer/models/custom_decoder.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, division, print_function
 import collections
 import tensorflow as tf
 from synthesizer.models.helpers import TacoTestHelper, TacoTrainingHelper
-from tensorflow.contrib.seq2seq.python.ops import decoder
-from tensorflow.contrib.seq2seq.python.ops import helper as helper_py
+from tensorflow_addons.seq2seq import decoder
+from tensorflow_addons.seq2seq import Sampler as helper_py
 from tensorflow.python.framework import ops, tensor_shape
 from tensorflow.python.layers import base as layers_base
 from tensorflow.python.ops import rnn_cell_impl
